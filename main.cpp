525cd81b38285c4c2afff8efaac82a5b772e46d9
Ben Zhang <benzhang1993@hotmail.com>
2018-09-21 11:21:57 -0700

    Added createFile and createList functionalities

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <random>
+#include <iomanip>
+#include <fstream>
+#include <array>
+
+using std:: cout;
+using std:: endl;
+
+struct pair {
+    int integer;
+    double decimal;
+};
+
+std::vector<pair> createList() {
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<> lengthDistribution(512, 1024);
+    std::uniform_real_distribution<> doubDistribution(50, 90);
+    int length = lengthDistribution(generator);
+
+    std::vector<pair> v;
+
+    for (int i = 0; i <= length; i++) {
+        double randomDouble = doubDistribution(generator);
+        pair temp = {i, randomDouble};
+        v.push_back(temp);
+        cout << v.at(i).integer << " " << std::setprecision(3)<< std::fixed << v.at(i).decimal << endl;
+    }
+
+    return v;
+}
+
+void createFile(std::vector<pair> list) {
+    std::ofstream myFile;
+    myFile.open("Readings.txt");
+    for (int i = 0; i <= list.size(); i++) {
+        myFile << list[i].integer << " " << std::setprecision(3)<< std::fixed << list[i].decimal << endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    createFile(createList());
     return 0;
 }
